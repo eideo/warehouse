b9edb0883ecd4a82a8823a28874cdc9349dfc726
roc <yinpengroc@gmail.com>
2016-10-18 16:57:54 +1300

    fix bugs

--- src/main/java/com/fh/service/auto/WooApiGetOrdersService.java
+++ src/main/java/com/fh/service/auto/WooApiGetOrdersService.java
@@ -1,6 +1,5 @@
 package com.fh.service.auto;
 
-
 import java.io.IOException;
 import java.util.List;
 //
@@ -29,207 +28,169 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-
-
 @Service("autoApiService")
 public class WooApiGetOrdersService {
-	private   String url=Const.url;
-	private   String username=Const.username;
-	private   String password=Const.password;
+	private String url = Const.url;
+	private String username = Const.username;
+	private String password = Const.password;
 	protected Logger logger = Logger.getLogger(this.getClass());
 	@Resource(name = "daoSupport")
 	private DaoSupport dao;
-   /**
-    * https://myalpha.co.nz/wp-json/wc/v1/orders?per_page=100&after=2016-09-22T14:01:54.9571247Z
-    * https://myalpha.co.nz/wp-json/wc/v1/orders?per_page=100&after=2016-09-28T17:35:32+1300
-    * @return
-    * @throws UnirestException
- * @throws IOException 
- * @throws JsonMappingException 
- * @throws JsonParseException 
-    */
-	private  List<Order> getOrdersFromAnfa() throws UnirestException, JsonParseException, JsonMappingException, IOException{
 
-		String date=DateUtil.getTimeISO_8601(DateUtil.getAfterDayDate(-1));
-	
-		HttpResponse<JsonNode>  response =Unirest.get(this.url+date).basicAuth(this.username, this.password).asJson();
-		
-		System.out.println(" :"+this.url+date);
-		System.out.println("username:"+this.username);
-		System.out.println("password:"+this.password);
-		
-		String jsonString =response.getBody().getArray().toString();
+	/**
+	 * https://myalpha.co.nz/wp-json/wc/v1/orders?per_page=100&after=2016-09-
+	 * 22T14:01:54.9571247Z
+	 * https://myalpha.co.nz/wp-json/wc/v1/orders?per_page=100&after=2016-09-
+	 * 28T17:35:32+1300
+	 * 
+	 * @return
+	 * @throws UnirestException
+	 * @throws IOException
+	 * @throws JsonMappingException
+	 * @throws JsonParseException
+	 */
+	private List<Order> getOrdersFromAnfa()
+			throws UnirestException, JsonParseException, JsonMappingException, IOException {
+
+		String date = DateUtil.getTimeISO_8601(DateUtil.getAfterDayDate(-1));
+
+		HttpResponse<JsonNode> response = Unirest.get(this.url + date).basicAuth(this.username, this.password).asJson();
+
+		System.out.println(" :" + this.url + date);
+		System.out.println("username:" + this.username);
+		System.out.println("password:" + this.password);
+
+		String jsonString = response.getBody().getArray().toString();
 		System.out.println(jsonString);
-		List<Order> lst=	JsonUtil.getListFromJson(jsonString, List.class, Order.class);
-		if(lst.isEmpty()){
-			
+		List<Order> lst = JsonUtil.getListFromJson(jsonString, List.class, Order.class);
+		if (lst.isEmpty()) {
+
 			return null;
 		}
-		         for (Order order : lst) {	        	
-		        	System.out.println(order.getBilling().getAddress_1()+order.getBilling().getPhone());
-		        	 List<Iterm> iterms=order.getLine_items();
-		        	if(iterms!=null){
-		        		for (Iterm iterm : iterms) {
-							iterm.getSku();
-//							System.out.println(iterm.getSku());
-//							System.out.println(iterm.getTotal());
-						}
-		        	}
-					
+		for (Order order : lst) {
+			System.out.println(order.getBilling().getAddress_1() + order.getBilling().getPhone());
+			List<Iterm> iterms = order.getLine_items();
+			if (iterms != null) {
+				for (Iterm iterm : iterms) {
+					iterm.getSku();
+					// System.out.println(iterm.getSku());
+					// System.out.println(iterm.getTotal());
 				}
+			}
+
+		}
 		return lst;
 	}
-	
-	
-	public void saveOrders(List<Order> l,int dept) throws Exception{
-		if(l!=null&&l.size()!=0){
-		for (Order order : l) {
-		//	dao.findForObject("WarehouseMapper.checkOrder", order);
-			//System.out.println(order.getId()+order.getDate_created());
-			order.setOriginal_ID(order.getId());
-			order.setDept_ID(dept);
-			
-			int m=(int)dao.findForObject("WarehouseMapper.checkOrder", order);
-			
-			
-			
-			
-		//	System.out.println(dao.findForObject("WarehouseMapper.checkOrder", order));
-			
-			if(m==0){
-			  String remark=order.getBilling().getCity()+","+order.getBilling().getCompany();
-			
-				order.setRemark(remark);
-				order.setDept_ID(dept);
-				dao.save("WarehouseMapper.saveOrder", order);
-				//System.out.println("productid======"+order.getId());
-				List<Iterm> iterms = order.getLine_items();
-				for (Iterm iterm : iterms) {
-					int productid =(int)dao.findForObject("WarehouseMapper.searchProduct", iterm.getSku());
-					//System.out.println("sku+++++++++++++++++++++++++"+iterm.getSku());
-					iterm.setOrder_ID(order.getId());
-					
-					iterm.setProduct_ID(productid);
-					//System.out.println("ppp===="+productid);
-					
-				}
-				dao.save("WarehouseMapper.saveOrderitems", iterms);
-				
-				
-			}
-			
-		}
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public void saveOrdersCommon(List<com.fh.entity.warehouse.orders.Order> l,int dept) throws Exception{
-		if(l!=null&&l.size()!=0){
-		for (com.fh.entity.warehouse.orders.Order order : l) {
-		//	dao.findForObject("WarehouseMapper.checkOrder", order);
-			//System.out.println(order.getId()+order.getDate_created());
-			order.setOriginal_ID(order.getId());
-			order.setDept_ID(dept);
-			
-			int m=(int)dao.findForObject("WarehouseMapper.checkOrder", order);
-			
-			
-			
-			
-		//	System.out.println(dao.findForObject("WarehouseMapper.checkOrder", order));
-			
-			if(m==0){
-			//  String remark=order.getBilling().getCity()+","+order.getBilling().getCompany();
-			
-			//	order.setRemark(remark);
-				//order.setDept_ID(dept);
-				//order.setId(null);
-				dao.save("WarehouseMapper.saveOrderCommon", order);
-				
-				System.out.println("saveOrderCommon第三方======"+order.getId());
-				List<Line_Items> iterms = order.getLine_items();
-				for (Line_Items iterm : iterms) {
-					int productid =(int)dao.findForObject("WarehouseMapper.searchProduct", iterm.getSku());
-					//System.out.println("sku+++++++++++++++++++++++++"+iterm.getSku());
-					iterm.setOrder_ID(order.getId());
-					
-					iterm.setProduct_ID(productid);
-					
-					//System.out.println("ppp===="+productid);
-					
-				}
-				dao.save("WarehouseMapper.saveOrderitems", iterms);
-				
-				
-			}else {
-				PageData pD =new PageData();
-				pD.put("Order_ID", m);
-				pD.put("Status", order.getStatus());
-				dao.update("WarehouseMapper.updateOrderstatus", pD);
-			}
-			
-		}
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-	
+	public void saveOrders(List<Order> l, int dept) throws Exception {
+		if (l != null && l.size() != 0) {
+			for (Order order : l) {
+				// dao.findForObject("WarehouseMapper.checkOrder", order);
+				// System.out.println(order.getId()+order.getDate_created());
+				order.setOriginal_ID(order.getId());
+				order.setDept_ID(dept);
+
+				int m = (int) dao.findForObject("WarehouseMapper.checkOrder", order);
+
+				// System.out.println(dao.findForObject("WarehouseMapper.checkOrder",
+				// order));
+
+				if (m == 0) {
+					String remark = order.getBilling().getCity() + "," + order.getBilling().getCompany();
+
+					order.setRemark(remark);
+					order.setDept_ID(dept);
+					dao.save("WarehouseMapper.saveOrder", order);
+					// System.out.println("productid======"+order.getId());
+					List<Iterm> iterms = order.getLine_items();
+					for (Iterm iterm : iterms) {
+						int productid = (int) dao.findForObject("WarehouseMapper.searchProduct", iterm.getSku());
+						// System.out.println("sku+++++++++++++++++++++++++"+iterm.getSku());
+						iterm.setOrder_ID(order.getId());
+
+						iterm.setProduct_ID(productid);
+						// System.out.println("ppp===="+productid);
+
+					}
+					dao.save("WarehouseMapper.saveOrderitems", iterms);
+
+				}
+
+			}
+		}
+	}
+
+	public void saveOrdersCommon(List<com.fh.entity.warehouse.orders.Order> l, int dept) throws Exception {
+		if (l != null && l.size() != 0) {
+			for (com.fh.entity.warehouse.orders.Order order : l) {
+				// dao.findForObject("WarehouseMapper.checkOrder", order);
+				// System.out.println(order.getId()+order.getDate_created());
+				order.setOriginal_ID(order.getId());
+				order.setDept_ID(dept);
+
+				int m = (int) dao.findForObject("WarehouseMapper.checkOrder", order);
+
+				// System.out.println(dao.findForObject("WarehouseMapper.checkOrder",
+				// order));
+
+				if (m == 0) {
+			
+					dao.save("WarehouseMapper.saveOrderCommon", order);
+
+					List<Line_Items> iterms = order.getLine_items();
+					for (Line_Items iterm : iterms) {
+						int productid = (int) dao.findForObject("WarehouseMapper.searchProduct", iterm.getSku());
+					
+						iterm.setOrder_ID(order.getId());
+
+						iterm.setProduct_ID(productid);
+
+
+					}
+					dao.save("WarehouseMapper.saveOrderitems", iterms);
+
+				} else {
+					PageData pD = new PageData();
+					pD.put("Order_ID", m);
+					pD.put("Status", order.getStatus());
+					dao.update("WarehouseMapper.updateOrderstatus", pD);
+				}
+
+			}
+		}
+	}
+
 	/**
 	 * 
 	 * @param time
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Order> getAnfaOrder(AnfiApi time) throws Exception{
-		List<Order> list=(List<Order>) dao.findForList("WarehouseMapper.getOrdersQuery", time);
-		System.out.println("list size is              "+list.size());
+	public List<Order> getAnfaOrder(AnfiApi time) throws Exception {
+		List<Order> list = (List<Order>) dao.findForList("WarehouseMapper.getOrdersQuery", time);
+		System.out.println("list size is              " + list.size());
 		return list;
-		
-		
-		
-	}
-	
-	public void sysOrderAnfaOrder() throws JsonParseException, JsonMappingException, UnirestException, IOException, Exception{
-		System.out.println("this method be invoked22");
-		this.saveOrders(this.getOrdersFromAnfa(),2);
-	}
-	
-	
-	public static void main(String[] args) {
-	try {
-		WooApiGetOrdersService wooApiGetOrders = new WooApiGetOrdersService();
-	wooApiGetOrders.getOrdersFromAnfa();
-	} catch (Exception e) {
-		// TODO: handle exception
-		e.printStackTrace();
-	}
-	
-	
-}
-			
 
-		
-			
-			//Unirest.get("http://httpbin.org/headers").basicAuth("username", "password").asJson();
+	}
+
+	public void sysOrderAnfaOrder()
+			throws JsonParseException, JsonMappingException, UnirestException, IOException, Exception {
+		System.out.println("this method be invoked22");
+		this.saveOrders(this.getOrdersFromAnfa(), 2);
+	}
+
+	public static void main(String[] args) {
+		try {
+			WooApiGetOrdersService wooApiGetOrders = new WooApiGetOrdersService();
+			wooApiGetOrders.getOrdersFromAnfa();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+	// Unirest.get("http://httpbin.org/headers").basicAuth("username",
+	// "password").asJson();
 
 }

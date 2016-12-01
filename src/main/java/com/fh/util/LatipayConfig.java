1e339dc5f3a00e94296c82800bf3181e0dfc8ba4
roc <yinpengroc@gmail.com>
2016-12-01 14:07:26 +1300

    fix bugs

--- src/main/java/com/fh/util/LatipayConfig.java
+++ src/main/java/com/fh/util/LatipayConfig.java
@@ -12,6 +12,11 @@ import java.util.Date;
  *以下代码只是为了方便商户测试而提供的样例代码，商户可以根据自己网站的需要，按照技术文档编写,并非一定要使用该代码。
  *该代码仅供学习和研究LATIPAY接口使用，只是提供一个参考。
  */
+/**
+ * 
+ * @author PENG
+ *
+ */
 public class LatipayConfig {
 	/**
      * MyRapidPay提供给商户的网关地址
